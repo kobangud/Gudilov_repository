bc6c22cd4be587140127beef0f36569ac4e922c6
kobangud <kobangud@mail.ru>
2022-11-24 15:09:30 +0300

    Добавил переменные к заданию №2 Типы данных

--- src/com/max/idea/Main.java
+++ src/com/max/idea/Main.java
@@ -3,7 +3,12 @@ package com.max.idea;
 import java.util.Arrays;
 
 public class Main {
+
     public static void main(String[] args) {
+
+        double num = 1234567.12345;
+        byte a = 127;
+
         System.out.println("Hello World");
     }
 }
