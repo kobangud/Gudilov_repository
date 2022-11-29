151fe50eb66499ee5dec08511c7c6284a536ffef
kobangud <kobangud@mail.ru>
2022-11-29 14:51:31 +0300

    Задача №3_Массивы
    Выполните следующие действия с массивом:
    1. Задайте массив из 5 любых целых чисел
    2. Поменяйте местами первый и последний элемент в массиве
    3. Вывести в консоль результат суммы первого и среднего элемента
    Примечание: Для получения последнего и среднего элементов в массиве используйте свойство length.

--- src/com/max/idea/Main.java
+++ src/com/max/idea/Main.java
@@ -1,14 +1,32 @@
 package com.max.idea;
 
+
 import java.util.Arrays;
 
 public class Main {
 
-    public static void main(String[] args) {
 
-        double num = 1234567.12345;
+    public static void main(String [] args ) {
+
+      /*  double num = 1234567.12345;
         byte a = 127;
 
         System.out.println("Hello World");
-    }
+
+        int[] array2 = {1,2,3,4,5,6,7,8,9};
+
+                for (int i = 0; i<array2.length; i++) {
+                    if (!(array2[i] % 2 == 0)){
+                        System.out.println(i);
+                    }
+       */
+
+        int[] array1 = {2,5,6,8,7};
+        int a = array1[0];
+        array1 [0] = array1[array1.length - 1];
+        array1[array1.length-1] = a;
+        System.out.println("Сумма первого и среднего элементов =  " + (array1[0] + array1[array1.length-3]));
+
+
+        }
 }
